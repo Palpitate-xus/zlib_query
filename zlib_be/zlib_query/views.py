7c2bc8b0c1506c71170e26c6f1e78cbffe0a332c
Palpitate-xus <xus1758011080@outlook.com>
2022-11-10 00:29:29 +0800

    添加全部信息

--- zlib_be/zlib_query/views.py
+++ zlib_be/zlib_query/views.py
@@ -14,6 +14,30 @@ def book_query(request):
     data = Books.objects.filter(title__contains = book_name)
     res = []
     for i in data:
-        res.append({'id':i.zlibrary_id,'title': i.title,'author': i.author,'torrent': i.pilimi_torrent})
+        res.append({
+            'id':i.zlibrary_id,
+            'title': i.title,
+            'author': i.author,
+            'torrent': i.pilimi_torrent,
+            'in_libgen': i.in_libgen,
+            'description': i.description,
+            'extension': i.extension,
+            'md5': i.md5,
+            'md5_reported': i.md5_reported,
+            'publisher': i.publisher,
+            'language': i.language,
+            'year': i.year,
+            'edition': i.edition,
+            'pages': i.pages,
+            'cover_url': i.cover_url,
+            'unavailable': i.unavailable,
+            'date_added': i.date_added,
+            'date_modified': i.date_modified,
+            'filesize': i.filesize,
+            'filesize_reported': i.filesize_reported,
+            'series': i.series,
+            'volume': i.volume
+            })
+        # res.append(i)
     response = {'code': 200, 'msg': 'success', 'data': res}
     return HttpResponse(json.dumps(response))
