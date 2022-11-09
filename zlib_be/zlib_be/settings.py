59069f1f493a7fa4ca3da3e2acd3345f6ae91a4d
Palpitate-xus <xus1758011080@outlook.com>
2022-11-10 00:05:35 +0800

    处理跨域

--- zlib_be/zlib_be/settings.py
+++ zlib_be/zlib_be/settings.py
@@ -131,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ORS_ORIGIN_ALLOW_ALL = True
