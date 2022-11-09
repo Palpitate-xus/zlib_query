a52e2447fb85bfa259f196e1688946331d7a100b
Palpitate-xus <xus1758011080@outlook.com>
2022-11-10 00:05:45 +0800

    添加查询功能

--- zlib_be/zlib_be/urls.py
+++ zlib_be/zlib_be/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, re_path
+from zlib_query.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('^api/test', test),
+    re_path('^api/book_query', book_query),
 ]
--- zlib_be/zlib_query/views.py
+++ zlib_be/zlib_query/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
+import json
+from zlib_query.models import *
+# Create your views here.
 
 # Create your views here.
+def test(request):
+    response = {'code': 200, 'msg': 'success'}
+    return HttpResponse(json.dumps(response))
+
+def book_query(request):
+    book_name = request.body.decode()
+    print(book_name)
+    data = Books.objects.filter(title__contains = book_name)
+    res = []
+    for i in data:
+        res.append({'id':i.zlibrary_id,'title': i.title,'author': i.author,'torrent': i.pilimi_torrent})
+    response = {'code': 200, 'msg': 'success', 'data': res}
+    return HttpResponse(json.dumps(response))
